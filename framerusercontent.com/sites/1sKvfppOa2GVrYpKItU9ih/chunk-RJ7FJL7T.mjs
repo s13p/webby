798c0acd362ec69365efc2765adee4edf3184f4b
s13p <142729653+s13p@users.noreply.github.com>
2024-02-07 10:38:58 +0300

    Initial commit

---
+++ framerusercontent.com/sites/1sKvfppOa2GVrYpKItU9ih/chunk-RJ7FJL7T.mjs
@@ -0,0 +1,2 @@
+import{a,c as o}from"./chunk-WCDE66F2.mjs";var r="default"in o?a:o,t={},e=r;t.createRoot=e.createRoot;t.hydrateRoot=e.hydrateRoot;var s=t.createRoot,f=t.hydrateRoot;export{s as a,f as b};
+//# sourceMappingURL=chunk-RJ7FJL7T.mjs.map
